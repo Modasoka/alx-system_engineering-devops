8ae1f6cb08758397c30ff601b84579bfbd8aa308
WambuaJoe <kaytujohn254@gmail.com>
2024-01-18 12:35:42 +0300

    code update - install nginx using Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # install nginx server w/ Puppet
 exec {'install':
 	provider => shell,
-	command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server-name _;/server_name;\n\trewrite ^\/redirect_me https:\/\/github.com\/WambuaJoe permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart',
+	command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/rky-ke permanent;/" /etc/nginx/sites-available/default ; sudo service nginx restart',
 }
