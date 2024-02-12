64cae348b32c258cf0f1614597a756c261a07deb
WambuaJoe <kaytujohn254@gmail.com>
2024-02-12 12:03:15 +0300

    gather employee data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-# create script using REST API to gather data
+"""
+returns information for a given employee ID
+about his/her TODO list progress
+"""
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    tot = 0
+    total = 0
     completed = 0
-    
-    employee_url = "https://jsonplaceholder.typicode.com/user/" + argv[1]
+    employee_url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
     tasks_url = "https://jsonplaceholder.typicode.com/todos"
     employee_id = int(argv[1])
-    
+
     employee_name = (requests.get(employee_url)).json().get("name")
     tasks = requests.get(tasks_url)
-    
     for task in tasks.json():
         if (task.get("userId") == employee_id):
-            tot += 1
+            total += 1
             if (task.get("completed")):
                 completed += 1
-        print("Employee {} is done with tasks({}/{})".format(
-                                                             employee_name,
-                                                             completed, total))
-        for task in tasks.json():
-            if (task.get("userId") == employee_id):
-                if (task.get("completed")):
-                    print("\t " + task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                          completed, total))
+    for task in tasks.json():
+        if (task.get("userId") == employee_id):
+            if (task.get("completed")):
+                print("\t " + task.get("title"))
