97935c73a12cab8b4c2dbc7853e2509f4ba6ec6f
WambuaJoe <kaytujohn254@gmail.com>
2024-03-25 09:57:49 +0300

    debug to find nginx error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# install flask
+package { 'python3-pip':
+    ensure  => 'installed',
+}
+
+exec { 'install-flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin'],
+    creates => '/usr/local/lib/python3.10/dist-packages/flask',
 }
