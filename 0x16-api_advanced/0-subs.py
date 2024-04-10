cfbcf0b9ad47a4c1e23ab94a6df063213110fb67
WambuaJoe <kaytujohn254@gmail.com>
2024-04-10 09:12:26 +0300

    code update - implement json

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,18 @@
 this module is used to retrieve subscriber count for a subreddit
 """
 import requests
-
+import json
 
 def number_of_subscribers(subreddit):
     """
     Retrieves the number of total subscribers of a subreddit
     """
-    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    header = {"User-Agent": "MyUser"}
-    response = requests.get(url, allow_redirects=False, headers=header)
-
-    if response.status_code == 404:
-        return (0)
-    if response.status_code == 200:
-        req = int(response.json().get("data").get("subscribers"))
-        return (req)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f'https://wwww.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code != 200:
+        return 0
+    
+    data = response.json()
+    return data.get("data").get("subscribers")
