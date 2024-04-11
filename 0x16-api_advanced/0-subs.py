519b46d70d124c5e794630d65f5db77d5d4213c3
WambuaJoe <kaytujohn254@gmail.com>
2024-04-11 09:26:52 +0300

    code update - fix input type

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,20 +3,18 @@
 this module is used to query the Reddit API in order to retrieve
 subscriber count for a given subreddit
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
     """
     Retrieves the number of total subscribers of a subreddit
     """
-    import requests
-
-    info_subreddit = requests.get("https://www.reddit.com/r/{}/about.json"
-                                  .format(subreddit),
-                                  headers={"User-Agent": "My-User-Agent"},
-                                  allow_redirects=False)
-
-    if info_subreddit.status_code >= 300:
+    headers = {"User-Agent": 'u/DepartureNo8863'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-
-    return info_subreddit.json().get("data").get("subscribers")
