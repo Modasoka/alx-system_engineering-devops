6d98f4bc193861ae6401bfe74abd0183d2dbb111
WambuaJoe <kaytujohn254@gmail.com>
2024-04-11 09:50:39 +0300

    code update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,26 +10,44 @@ def recurse(subreddit, hot_list=[], params={}):
     """
     Returns a list of titles of all hot posts of a subreddit
     """
-    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
-    header = {"User-Agent": "MyUser"}
+    # url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
+    # header = {"User-Agent": "MyUser"}
 
-    response = requests.get(url, allow_redirects=False,
-                            headers=header, params=params)
-    data = response.json().get("data")
+    # response = requests.get(url, allow_redirects=False,
+    #                         headers=header, params=params)
+    # data = response.json().get("data")
 
-    if response.status_code == 404:
-        return(None)
+    # if response.status_code == 404:
+    #     return(None)
 
-    after = data.get("after")
-    before = data.get("before")
+    # after = data.get("after")
+    # before = data.get("before")
 
-    if response.status_code == 200:
-        req = data.get("children")
-        for item in req:
-            hot_list.append(item.get("data").get("title"))
+    # if response.status_code == 200:
+    #     req = data.get("children")
+    #     for item in req:
+    #         hot_list.append(item.get("data").get("title"))
 
-    if after is None:
-        return(hot_list)
+    # if after is None:
+    #     return(hot_list)
 
-    return recurse(subreddit, hot_list,
-                   params={"after": after, "before": before})
+    # return recurse(subreddit, hot_list,
+    #                params={"after": after, "before": before})
+
+    headers = {
+        "User-Agent": "linux:myserver:v5.11.0 (by /u/linda)"
+    }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        url += "?after={}".format(after)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    data = response.json()
+    titles = data['data']['children']
+    for title in titles:
+        hot_list.append(title['data']['title'])
+    after = data['data']['after']
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
